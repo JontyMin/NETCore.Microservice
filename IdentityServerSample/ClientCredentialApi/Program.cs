bc3f7e2f036e78af95bd3021e7a7260290ceb095
JontyWang <501211312@qq.com>
2021-03-14 23:46:47 +0800

    :heavy_plus_sign: IdentityServer

--- IdentityServerSample/ClientCredentialApi/Program.cs
+++ IdentityServerSample/ClientCredentialApi/Program.cs
@@ -7,7 +7,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 
-namespace IdentityServerCenter
+namespace ClientCredentialApi
 {
     public class Program
     {
@@ -20,7 +20,6 @@ namespace IdentityServerCenter
             Host.CreateDefaultBuilder(args)
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
-                    webBuilder.UseUrls("http://localhost:5001");
                     webBuilder.UseStartup<Startup>();
                 });
     }
---
+++ IdentityServerSample/IdentityServerCenter/Config.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections.Generic;
+using IdentityServer4.Models;
+
+namespace IdentityServerCenter
+{
+    public class Config
+    {
+        public static IEnumerable<ApiResource> GetResources()
+        {
+            return new List<ApiResource>()
+            {
+                new ApiResource("api","My Api")
+            };
+        }
+
+        public static IEnumerable<Client> GetClients()
+        {
+            return new List<Client>()
+            {
+                new Client()
+                {
+                    ClientId = "client",
+                    AllowedGrantTypes = GrantTypes.ClientCredentials,
+                    ClientSecrets ={ new Secret("secret".Sha256())},
+                    AllowedScopes = {"api"}
+                } 
+            };
+        }
+    }
+}
