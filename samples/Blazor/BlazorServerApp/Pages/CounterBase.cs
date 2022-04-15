0a08655c15e8b4ba55837997b885ee4bbedb66fb
Jonty <501211312@qq.com>
2022-04-15 23:27:02 +0800

    base class

---
+++ samples/Blazor/BlazorServerApp/Pages/CounterBase.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace BlazorServerApp.Pages;
+
+public class CounterBase:ComponentBase
+{
+    protected int currentCount = 0;
+
+    protected void IncrementCount()
+    {
+        currentCount+=2;
+    }
+}
